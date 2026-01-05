acd281fe6f5744bd9a3deaf5d233687e81143e7d
isandypuertas <42593401+isandypuertas@users.noreply.github.com>
2026-01-05 18:13:03 +0100

    Add files via upload

---
+++ AdvancedCSharp_Reflection/AdvancedCSharp_Reflection/Program.cs
@@ -0,0 +1,143 @@
+﻿using System.Data.Common;
+using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+namespace AdvancedCSharp_Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*insert here your dll path, not mine*/
+            var assembly = Assembly.LoadFrom(@"C:\Users\isabelle.puertas\source\repos\AdvancedCSharp_Reflection\PrintAssemblyReference\bin\Debug\net8.0\PrintAssemblyReference.dll");
+
+            foreach (var type in assembly.GetTypes())
+            {
+                //WriteAllInformation(type);
+                ActivateMethods(type);
+            }
+        }
+
+        private static void ActivateMethods(Type type)
+        {
+            Console.WriteLine($"Type: {type.FullName}");
+            Console.WriteLine("-----------------------------------------");
+            var instance = Activator.CreateInstance(type);
+
+            // | is used to check it all
+            foreach (var field in type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                Console.WriteLine($"Field: {field.Name} Value set: myValue");
+                field.SetValue(instance, "myValue");
+
+                Console.WriteLine($"    Field: {field.Name} Value: {field.GetValue(instance)}");
+            }
+            Console.WriteLine("-----------------------------------------");
+
+            foreach (var property in type.GetProperties())
+            {
+                //checking static property (not needed)
+                //bool isStatic = property.GetMethod?.IsStatic == true || property.SetMethod?.IsStatic == true;
+                //object value = property.GetValue(isStatic ? null : instance);
+
+                Console.WriteLine($"Property: {property.Name}");
+                Console.WriteLine($"        Property Value: {property.GetValue(instance)}");
+                Console.WriteLine();
+            }
+            Console.WriteLine("-----------------------------------------");
+
+            foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                if (method.IsSpecialName)
+                {
+                    Console.WriteLine($"Internal Method: {method.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"Method: {method.Name}");
+
+                    if (method.GetParameters().Length > 0)
+                    {
+                        List<object> parameters = new List<object>();
+
+                        foreach (var parameter in method.GetParameters())
+                        {
+                            object typeImplicitDefaultValue = null;
+                            if (parameter.ParameterType.IsValueType) /*int, double...*/
+                            {
+                                typeImplicitDefaultValue = Activator.CreateInstance(parameter.ParameterType);
+                                Console.WriteLine($"    Parameter: {parameter.Name} Type: {parameter.ParameterType} Default Value (by type): {typeImplicitDefaultValue}");
+                            }
+                            else if (parameter.ParameterType == typeof(string)) /*!parameter.ParameterType.IsValueTyp - class, string...*/
+                            {
+                                typeImplicitDefaultValue = parameter.DefaultValue;
+                                Console.WriteLine($"    Parameter: {parameter.Name} Type: {parameter.ParameterType} Default Value (by definition): {typeImplicitDefaultValue}");
+                            }
+
+                            //same as above
+                            //object typeImplicitDefaultValue = parameter.ParameterType.IsValueType ? Activator.CreateInstance(parameter.ParameterType) : null;
+
+                            parameters.Add(typeImplicitDefaultValue);
+                        }
+
+                        Console.WriteLine($"        Invoke results:");
+                        method.Invoke(instance, parameters.ToArray());
+                    }
+                    else if (method.ReturnType.Name != "Void")
+                    {
+                        var returnedValue = method.Invoke(instance, null);
+                        Console.WriteLine($"        Invoke results:");
+                        Console.WriteLine($"            Returned value: {returnedValue}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"        Invoke results:");
+                        method.Invoke(instance, null);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("-----------------------------------------");
+        }
+
+        private static void WriteAllInformation(Type type)
+        {
+            Console.WriteLine($"Type: {type.FullName}");
+            Console.WriteLine("-----------------------------------------");
+
+            // | is used to check it all
+            foreach (var field in type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                Console.WriteLine($"Field: {field.Name}");
+            }
+            Console.WriteLine("-----------------------------------------");
+
+            foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                if (method.IsSpecialName)
+                {
+                    Console.WriteLine($"Internal Method: {method.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"Method: {method.Name}");
+                }
+
+                foreach (var parameter in method.GetParameters())
+                {
+                    Console.WriteLine($"    Parameter: {parameter.Name} Type: {parameter.ParameterType}");
+                }
+            }
+            Console.WriteLine("-----------------------------------------");
+
+            foreach (var property in type.GetProperties())
+            {
+                Console.WriteLine($"Property: {property.Name}");
+            }
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
---
+++ AdvancedCSharp_Reflection/PrintAssemblyReference/ReflectionPrint.cs
@@ -0,0 +1,38 @@
+﻿namespace PrintAssemblyReference
+{
+    public class ReflectionPrint
+    {
+        private string privateField;
+        public string PrivateProperty => privateField;
+        public static string StaticPublicProperty => "Static Public Property Value";
+        public void Print()
+        {
+            Console.WriteLine($"            Writing from Print class");
+        }
+
+        public string GetValue()
+        {
+            return this.privateField;
+        }
+
+        public void PrintValue()
+        {
+            Console.WriteLine($"            Field value: {this.privateField}");
+        }
+
+        public void PrintValueWithParameter(string parameter = "default_defined_on_method")
+        {
+            Console.WriteLine($"            Parameter value: {parameter}");
+        }
+
+        public void PrintValueWithMultipleParameters(string parameter1 = "param1_default_value", int parameter2 = 10, double parameter3 = 10)
+        {
+            Console.WriteLine($"            Parameters values parameter1:{parameter1} parameter2-{parameter2} parameter3-{parameter3}");
+        }
+
+        private void PrintPrivate()
+        {
+            Console.WriteLine($"            Writing from private method");
+        }
+    }
+}
